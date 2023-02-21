4d5b529dcabffd267d55485dafacf0bed00a832f
diisk <diisk@hotmail.com>
2023-02-21 20:46:14 -0300

    finished lesson 2

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,13 @@
 import Fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
 import cors from '@fastify/cors'
+import { prisma } from './lib/prisma';
+import { appRoutes } from './routes';
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
-app.register(cors)// QUALQUER APLICAÇÃO PODE CONSUMIR A API, CONFIGURÁVEL
 
-app.get('/', () => {
-    const habits = prisma.habit.findMany();
-
-    return habits;
-});
+app.register(cors); // QUALQUER APLICAÇÃO PODE CONSUMIR A API, CONFIGURÁVEL
+app.register(appRoutes);
 
 app.listen({
     port: 3333
